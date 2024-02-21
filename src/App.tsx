92c4be95fde347e182e814a245be32ae0b3cb028
shreyasprabhakar <shrprabh@ttu.edu>
2024-02-20 21:50:51 -0600

    Prepare for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import FCFSPage from "./components/FSFSpage"; // Corrected the file name based on your comment
 import SJFPage from "./components/SJFpage";
 
